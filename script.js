a4099a28cb7733429eb9f871cb97aca4e5fc5d10
ekunkoya.seun@gmail.com <ekunkoya.seun@gmail.com>
2019-05-11 14:19:30 +0100

    added a keypress event listener to input field

--- script.js
+++ script.js
@@ -15,7 +15,18 @@ button.addEventListener("click", function(){
       var li = document.createElement('li');
       li.appendChild(document.createTextNode(input.value));
       ul.appendChild(li);
+      input.value = '';
   }
   //console.log(input.value);
+})
 
+input.addEventListener("keypress", function(event){
+  if (input.value.length > 0 && event.keyCode === 13) {
+    //create an element li since we want to add to the list
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(input.value));
+      ul.appendChild(li);
+      input.value = '';
+  }
+  //console.log(input.value);
 })
