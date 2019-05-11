e6273404c9e92f54d50bb1b9b5aef4c1ec19f4c3
ekunkoya.seun@gmail.com <ekunkoya.seun@gmail.com>
2019-05-11 14:04:52 +0100

    add event to the button to accept input which is added to the list items

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-var button = document.getElementsByTagName("Button")[0]
+// var button = document.getElementsByTagName("Button")[0]
+//
+// button.addEventListener("click", function() {
+//   console.log("CLICK!!!");
+// })
 
-button.addEventListener("click", function() {
-  console.log("CLICK!!!");
+var button = document.getElementById('enter');
+var input = document.getElementById('userinput');
+var ul = document.querySelector('ul');
+
+//add event listener to the Button
+button.addEventListener("click", function(){
+  console.log(input.value);
+  //create an element li since we want to add to the list
+  var li = document.createElement('li');
+  li.appendChild(document.createTextNode(input.value));
+  ul.appendChild(li);
 })
