cda9bf9e32e271ddb7dbbb3d75612d9eb7f2d375
ekunkoya.seun@gmail.com <ekunkoya.seun@gmail.com>
2019-05-11 14:10:38 +0100

    added a test to make sure input values are not empty

--- script.js
+++ script.js
@@ -10,9 +10,12 @@ var ul = document.querySelector('ul');
 
 //add event listener to the Button
 button.addEventListener("click", function(){
-  console.log(input.value);
-  //create an element li since we want to add to the list
-  var li = document.createElement('li');
-  li.appendChild(document.createTextNode(input.value));
-  ul.appendChild(li);
+  if (input.value.length > 0) {
+    //create an element li since we want to add to the list
+      var li = document.createElement('li');
+      li.appendChild(document.createTextNode(input.value));
+      ul.appendChild(li);
+  }
+  //console.log(input.value);
+
 })
