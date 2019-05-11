674d07ea3a6fdb9dcc62adadafd707e764729955
ekunkoya.seun@gmail.com <ekunkoya.seun@gmail.com>
2019-05-11 14:31:24 +0100

    refactored codes to incorporate DRY

--- script.js
+++ script.js
@@ -8,25 +8,28 @@ var button = document.getElementById('enter');
 var input = document.getElementById('userinput');
 var ul = document.querySelector('ul');
 
+function inputLength(){
+  return input.value.length;
+}
+
+function createListElement(){
+  //create an element li since we want to add to the list
+    var li = document.createElement('li');
+    li.appendChild(document.createTextNode(input.value));
+    ul.appendChild(li);
+    input.value = '';
+    //console.log(input.value);
+}
+
 //add event listener to the Button
 button.addEventListener("click", function(){
-  if (input.value.length > 0) {
-    //create an element li since we want to add to the list
-      var li = document.createElement('li');
-      li.appendChild(document.createTextNode(input.value));
-      ul.appendChild(li);
-      input.value = '';
+    if (inputLength() > 0) {
+      createListElement();
   }
-  //console.log(input.value);
 })
 
 input.addEventListener("keypress", function(event){
-  if (input.value.length > 0 && event.keyCode === 13) {
-    //create an element li since we want to add to the list
-      var li = document.createElement('li');
-      li.appendChild(document.createTextNode(input.value));
-      ul.appendChild(li);
-      input.value = '';
+  if (inputLength() > 0 && event.keyCode === 13) {
+    createListElement();
   }
-  //console.log(input.value);
 })
